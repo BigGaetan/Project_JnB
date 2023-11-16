6b59432fa83c3dba7a819408185511e92d3511fc
BigGaetan <150789576+BigGaetan@users.noreply.github.com>
2023-11-16 13:30:58 +0100

    Update main.cpp
    on a mélanger les codes maintennant faut modifier la classe JNB

--- projet JNB/main.cpp
+++ projet JNB/main.cpp
@@ -2,13 +2,36 @@
 #include "JnB.h"
 #include "Bullet.h"
 #include "Enemy.h"
+#include "carte.h"
+#include <unordered_map>
+#include <iostream>
 
 const float PI = 3.14159265f;
 const float X_THRESHOLD = 100.0f;
 const float SCREEN_WIDTH = 800.0f;
 const float SCREEN_HEIGHT = 600.0f;
 const int MAX_BULLETS = 1;
+// code pour creer les collision
+void drawCollisionMap(sf::RenderWindow& window, const std::vector<std::vector<int>>& collisionMap, int tileSize) {
+    for (size_t i = 0; i < collisionMap.size(); ++i) {
+        for (size_t j = 0; j < collisionMap[i].size(); ++j) {
+            if (collisionMap[i][j] == 1) {
+                sf::RectangleShape rectangle(sf::Vector2f(tileSize, tileSize));
+                rectangle.setPosition(j * tileSize, i * tileSize);
+                rectangle.setFillColor(sf::Color::Red); // Couleur de la collision
+                window.draw(rectangle);
+            }
+            else{
+                sf::RectangleShape rectangle(sf::Vector2f(tileSize, tileSize));
+                rectangle.setPosition(j * tileSize, i * tileSize);
+                rectangle.setFillColor(sf::Color::Green); // Couleur de la collision
+                window.draw(rectangle);
+            }
+        }
+    }
+}
 
+//boucle main
 int main() {
     // Création de la fenêtre
     sf::RenderWindow window(sf::VideoMode(800, 600), "Mon Jeu");
@@ -19,7 +42,21 @@ int main() {
     // Création d'un vecteur d'ennemis
     std::vector<Enemy> enemies;
 
-    // Boucle principale du jeu
+  
+        Carte carte("maps/carte2.png","maps/carte3.txt");
+        std::vector<std::vector<int>> tileMap = createTileMapFromFile("maps/carte3.txt");
+        // Chargez le masque de collision
+        std::unordered_map<int, std::vector<std::vector<int>>> collisionMasks = createCollisionMasks();
+        std::vector<std::vector<int>> collisionMap = createTileMapCollision(tileMap);
+
+         // Affichage de la carte de collision résultante
+        /*for (const std::vector<int>& row : collisionMap) {
+            for (const int &value : row) {
+                std::cout << value << " ";
+            }
+            std::cout << std::endl;
+        }*/
+      // Boucle principale du jeu
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -29,13 +66,12 @@ int main() {
         }
 
         // Mise à jour de JnB (le joueur)
-        player.update(window);
+        player.update(collisionMap);
 
         // Mise à jour des ennemis
         for (Enemy& enemy : enemies) {
             enemy.update();
         }
-
         // Vérification des collisions entre les balles et les ennemis
         player.checkCollisionsWithEnemies(enemies);
 
