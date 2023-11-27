69725c0f8be65da0211e8ce5ba0f95d2d0f67e8f
mohamedtns <138494838+mohamedtns@users.noreply.github.com>
2023-11-27 21:17:59 +0100

    Delete Project_JnB-main/projet JNB/projet JNB directory

--- Project_JnB-main/projet JNB/projet JNB/maps/sans-titre.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset2" tilewidth="30" tileheight="32" tilecount="210" columns="15">
- <image source="tilled.jpeg" width="474" height="474"/>
-</tileset>
